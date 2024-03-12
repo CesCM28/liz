43be7d034a1b1cd8d561cdd89a6726e03016d5b1
CesCM28 <84432616+CesCM28@users.noreply.github.com>
2024-03-12 16:06:56 -0600

    interface y comentarios

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const myArray = ['Que bonitos ojos tienes!!', 
     'Que carnosos labios, hermosa!!', 
     'Me encantan tus chinos!!',
+    'Como me gusta tu piel, tan cálida y suave!!',
+    'Que linda te vez hoy, fiu fiu!!',
+    'uuuuuh la la, que hermosa estas!!',
 ];
 
 const meEncantaList = ['Que te ponías a perrear y moverme la colita!!', 
     'Tu forma de contar los cuentos!!', 
     'Como me miras!!',
+    'La paciencia que tienes!!',
+    'Cuando pestañeas de manera coqueta, me derrite!!'
 ];
   
 var mensaje = document.getElementById("mensaje");
@@ -14,7 +19,6 @@ document.getElementById("piropos").onclick = () => {
     const randomElement = myArray[Math.floor(Math.random() * myArray.length)]; 
     mensaje.innerHTML = randomElement;
 };
-
 
 document.getElementById("meEncanta").onclick = () => {
     const randomElement = meEncantaList[Math.floor(Math.random() * myArray.length)]; 
