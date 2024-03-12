8325b5fc4d6ec4b7fcaa31a85b4e3a933414ecbb
CesCM28 <84432616+CesCM28@users.noreply.github.com>
2024-03-11 21:49:28 -0600

    nuevo boton

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ const myArray = ['Que bonitos ojos tienes!!',
     'Que carnosos labios, hermosa!!', 
     'Me encantan tus chinos!!',
 ];
+
+const meEncantaList = ['Que te ponías a perrear y moverme la colita!!', 
+    'Tu forma de contar los cuentos!!', 
+    'Como me miras!!',
+];
   
 var mensaje = document.getElementById("mensaje");
   
 document.getElementById("piropos").onclick = () => {
     const randomElement = myArray[Math.floor(Math.random() * myArray.length)]; 
     mensaje.innerHTML = randomElement;
-    console.log("entro");
 };
 
+
+document.getElementById("meEncanta").onclick = () => {
+    const randomElement = meEncantaList[Math.floor(Math.random() * myArray.length)]; 
+    mensaje.innerHTML = randomElement;
+};
