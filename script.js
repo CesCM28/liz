159d25cbd6621c0e30fe77a41c53e75ff94b84b1
CesCM28 <84432616+CesCM28@users.noreply.github.com>
2024-03-12 19:31:29 -0600

    cambios esteticos

--- script.js
+++ script.js
@@ -10,17 +10,20 @@ const meEncantaList = ['Que te ponías a perrear y moverme la colita!!',
     'Tu forma de contar los cuentos!!', 
     'Como me miras!!',
     'La paciencia que tienes!!',
-    'Cuando pestañeas de manera coqueta, me derrite!!'
+    'Cuando pestañeas de manera coqueta, me derrite!!',
 ];
+
+const emojis = ['&#128513;', '&#128516;', '&#128521;', '&#128522;', '&#128517;']
   
 var mensaje = document.getElementById("mensaje");
   
 document.getElementById("piropos").onclick = () => {
-    const randomElement = myArray[Math.floor(Math.random() * myArray.length)]; 
-    mensaje.innerHTML = randomElement;
+    const randomElement = myArray[Math.floor(Math.random() * myArray.length)];
+    const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+    mensaje.innerHTML = randomElement + " " + randomEmoji;
 };
 
 document.getElementById("meEncanta").onclick = () => {
-    const randomElement = meEncantaList[Math.floor(Math.random() * myArray.length)]; 
+    const randomElement = meEncantaList[Math.floor(Math.random() * meEncantaList.length)]; 
     mensaje.innerHTML = randomElement;
 };
