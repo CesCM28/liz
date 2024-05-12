cde94a2a3d02a74a530f0a958f95e15e6ab0c57b
CesCM28 <84432616+CesCM28@users.noreply.github.com>
2024-05-11 18:56:14 -0600

    cambios del boton y mensajes

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const myArray = ['Que bonitos ojos tienes!!',
     'Que linda te vez hoy, fiu fiu!!',
     'uuuuuh la la, que hermosa estas!!',
     'Me encanta tu sonrisa!!',
+    'Que hermosas piernas!!',
+    'Que rico hueles!! (si en este momento)',
 ];
 
 const meEncantaList = ['Que te ponías a perrear y moverme la colita!!', 
@@ -12,12 +14,16 @@ const meEncantaList = ['Que te ponías a perrear y moverme la colita!!',
     'Como me miras!!',
     'La paciencia que tienes!!',
     'Cuando pestañeas de manera coqueta, me derrite!!',
-    'Que te gusta tener limpio y ordenado!!'
+    'Que te gusta tener limpio y ordenado!!',
+    'Compartir tiempo contigo y el vieje a Torreón, uff!!',
+    'Cuando te arreglas y me preguntas como te vez y haces esa mirada coqueta, me derrite!!',
 ];
 
 const emojis = ['&#128513;', '&#128516;', '&#128521;', '&#128522;', '&#128517;']
   
 var mensaje = document.getElementById("mensaje");
+
+var pass = document.getElementById("pass");
 
 var piropo = document.getElementById("piropos").onclick = () => {
     print(myArray);
@@ -25,6 +31,22 @@ var piropo = document.getElementById("piropos").onclick = () => {
 
 var encanta = document.getElementById("meEncanta").onclick = () => {
     print(meEncantaList);
+};
+
+document.getElementById("acceso").onclick = () => {
+    if (pass.value === ""){
+        pass.hidden = false;
+        acceso.innerText = "prueba";
+    } else {
+        if(pass.value === "esencia")
+            mensaje.innerHTML = "¿Te quieres casar conmigo?";
+        else         
+            mensaje.innerHTML = "Lo siento, es no es!!";
+        
+        pass.hidden = true;
+        acceso.innerHTML = "ya esta!!";
+        pass.value = "";
+    }
 };
 
 function print(array) {
